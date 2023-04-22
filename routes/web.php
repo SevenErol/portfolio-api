3cea440f1c30867d2ff99a642b1259ae7f3f18a5
SevenErol <lorenzo.maggi1998@gmail.com>
2023-04-22 11:36:06 +0200

    MVC structure created

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    // Route::resource('/project', ProjectController::class);
+    Route::resource('/project', ProjectController::class);
 });
 
 require __DIR__ . '/auth.php';
